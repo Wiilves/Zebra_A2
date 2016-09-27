867635ce0a1ee39cb5a4414e1c53f3b97b036981
Nikke Wiik <nikke.wiik@gmail.com>
2016-09-27 19:43:30 +0200

    third commit

--- app/app.component.js
+++ app/app.component.js
@@ -9,13 +9,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Horse = (function () {
+    function Horse() {
+    }
+    return Horse;
+}());
+exports.Horse = Horse;
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Horses';
+        this.horse = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Zebra Angular 2</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>{{horse.name}} details!</h2>\n    <div><label>id: </label>{{horse.id}}</div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"horse.name\" placeholder=\"name\">\n    </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,26 @@
 
 import { Component } from '@angular/core';
+export class Horse {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'my-app',
-  template: '<h1>Zebra Angular 2</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{horse.name}} details!</h2>
+    <div><label>id: </label>{{horse.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="horse.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Horses';
+  horse: Horse = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
